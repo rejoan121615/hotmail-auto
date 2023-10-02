813d8c97ed90f32cd7b3e3ca55a215fd0aa1bcf8
Mohd Rejoan <wearecrx@gmail.com>
2023-10-02 04:07:40 -0700

    eneter password added

--- src/app.js
+++ src/app.js
@@ -1,39 +1,63 @@
 const { chromium } = require("playwright");
 
+let emailList = [
+  "hzmzngon83",
+  "fginvsfb45",
+  "smvcneal49",
+  "ysrjladq73",
+  "gbbqeeuj51",
+  "sivllcsw83",
+  "uqxiyotx84",
+  "owoyscqd84",
+  "ryntpdds18",
+  "yirefpke2",
+];
 
-let details = {
-    name: 'something',
-    password: 'a123QWerd$32023',
-};
+var counter = 0;
+
+let password = "a123QWerd$32023";
+
 
 (async () => {
-    const browser = await chromium.launch({headless: false});
-    // goto hotmail 
-    const createMailPage = await browser.newPage();
-    await createMailPage.goto('https://signup.live.com/signup');
-    const createAccoLink = await createMailPage.getByRole('link', {name: 'Get a new email address'});
-    await createAccoLink.click();
-    // type a name 
-    const newEmailField = await createMailPage.locator('input').first({name: "MemberName"});
-    await newEmailField.click();
-    // type email 
-    async function TypeEmail ( emailAddress = details.name ) {
-        await createMailPage.keyboard.type( emailAddress, { delay: 100});
-        await createMailPage.keyboard.press('Enter');
-    }
+  const browser = await chromium.launch({ headless: false });
+  // goto hotmail
+  const createMailPage = await browser.newPage();
+  await createMailPage.goto("https://signup.live.com/signup");
+  const createAccoLink = await createMailPage.getByRole("link", {
+    name: "Get a new email address",
+  });
+  await createAccoLink.click();
+  // type a name
+  const newEmailField = await createMailPage
+    .locator("input")
+    .first({ name: "MemberName" });
+  await newEmailField.click();
+  // type email
+  async function TypeEmail(emailAddress = emailList[counter]) {
+    await createMailPage.keyboard.type(emailAddress, { delay: 100 });
+    await createMailPage.keyboard.press("Enter");
+  }
 
-    // enter mail address 
-    await TypeEmail();
-    
-    // wait for the message appeir after pressing enter 
-    try {
-        await createMailPage.waitForSelector('text=Someone already has this email address', { timeout: 5000});
-        console.log('email already available, try another email');
-        TypeEmail()
-    } catch {
-        console.log('email not available , type your password');
-    }
+  async function TypePassword (pass = password) {
+    await createMailPage.keyboard.type(pass, { delay: 100 });
+    await createMailPage.keyboard.press("Enter");
+  }
+  // enter mail address
+  await TypeEmail();
 
+  // wait for the message appeir after pressing enter
+  try {
+    await createMailPage.waitForSelector(
+      "text=Someone already has this email address",
+      { timeout: 5000 }
+    );
+    console.log("email already available, try another email");
+    counter++;
+    TypeEmail();
+  } catch {
+    // waiting for password filed to appear 
+    const passField = await createMailPage.waitForSelector('#PasswordInput', { timeout: 5000});
+    passField.click();
+    TypePassword();
+  }
 })();
-
-
